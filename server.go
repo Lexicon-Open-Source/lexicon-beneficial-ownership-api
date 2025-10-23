4a757703207ae92b9d343d160b044626a5c2f4df
Adryan Eka Vandra <adryanekavandra@gmail.com>
2025-10-23 20:59:31 +0700

    feat: add new allowed origin

--- server.go
+++ server.go
@@ -25,7 +25,7 @@ func NewLexiconBOServer(cfg config) (*LexiconBOServer, error) {
 	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
 	r.Use(cors.Handler(cors.Options{
 		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"https://bo.lexicon.id", "http://localhost:3000"},
+		AllowedOrigins: []string{"https://bo.lexicon.id", "https://beneficialowner.lexicon.id", "http://localhost:3000"},
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-API-KEY", "X-ACCESS-TIME", "X-REQUEST-SIGNATURE", "X-API-USER", "X-REQUEST-IDENTITY"},
